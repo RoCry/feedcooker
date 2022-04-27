ae084247cbc7cd17328ce750cf69fb05fc69a363
RoCry <crysheen@gmail.com>
2022-04-27 19:03:15 +0800

    feat: support parse json feed

--- cooker.py
+++ cooker.py
@@ -1,4 +1,5 @@
 import datetime
+import dateutil.parser
 from typing import Type, List
 
 import feedparser
@@ -62,9 +63,7 @@ class Cooker(object):
 
         if content_type.startswith('application/json'):
             feed = resp.json()
-            logger.debug(f"json feed: {feed}")
-            # TODO: try parse as json feed
-            return []
+            return [self._json_feed_to_feed_item(feed, item) for item in feed["items"]]
 
         f = feedparser.parse(resp.text)
         if f is None or f["bozo"]:
@@ -74,8 +73,46 @@ class Cooker(object):
         return [self._entry_to_feed_item(f, e) for e in f.entries]
 
     @staticmethod
-    def _json_feed_to_feed_item(feed: dict, item: dict) -> dict:
-        raise Exception("Not implemented")
+    def _json_feed_to_feed_item(feed: dict, e: dict) -> dict:
+        item = {
+            "title": e["title"],
+            "link": e["url"],
+            "unique_id": e["id"],
+        }
+
+        summary = e.get("summary")
+        content = e.get("content_html") if e.get("content_html") else e.get("content")
+
+        if content:
+            # prefer use content as description
+            item["description"] = content
+            item["content"] = content
+        elif summary:
+            item["description"] = summary
+        else:
+            item["description"] = ""
+
+        author_detail = (
+            e.get("author")
+            if e.get("author")
+            else feed.get("author")
+        )
+        if author_detail:
+            item["author_name"] = author_detail.get("name")
+            item["author_link"] = author_detail.get("url")
+
+        pubdate = e.get("date_published") if e.get("date_published") else e.get("date_modified")
+        if pubdate:
+            item["pubdate"] = dateutil.parser.parse(pubdate)
+        else:
+            item["pubdate"] = datetime.datetime.now()
+
+        update = e.get("date_modified")
+        if update:
+            item["update"] = dateutil.parser.parse(update)
+
+        logger.debug(f"item: {item}")
+        return item
 
     # mapping rss/atom entry to JSONFeed item(using in JSONFeed.add_item)
     @staticmethod
