2507814e22bc89161be45fecc0c03647c4f8bd29
RoCry <crysheen@gmail.com>
2022-04-27 17:23:34 +0800

    feat: update readme

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Cook multi feeds into one using GitHub Actions.
 # Setup
 
 - Fork or create repo with this repo as a template.
-- Update recipes.py (see `recipes.example.py`)
+- Update recipes.py (see [recipes.example.py](./recipes.example.py))
 - Uncomment schedule in `.github/workflows/main.yml` if you want to run it automatically.
--- recipes.py
+++ recipes.py
@@ -1,10 +1,3 @@
 recipes = {
-    "demo": {
-        "urls": [
-            # "https://benmccormick.org/feed.json",
-            "https://newsletter.banklesshq.com/feed",
-            "https://mirror.xyz/0x31d61b448700D321536FF36A2cd299a1dea14CA2/feed/atom",
-            "https://vitalik.ca/feed.xml",
-        ],
-    },
+    # Add recipes here, check recipes.example.py
 }
