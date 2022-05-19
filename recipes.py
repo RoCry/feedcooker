7db68edb3da82d45ef5269d10e653b97d6567f5d
RoCry <crysheen@gmail.com>
2022-05-19 15:49:00 +0800

    prefer submirror

--- recipes.py
+++ recipes.py
@@ -26,12 +26,38 @@ _recipes = {
 }
 
 
-def get_recipes() -> [dict]:
+def get_recipes(prefer_submirror=True) -> [dict]:
     recipes = {}
     for name, recipe in _recipes.items():
         for sub_name, r in _fulfill_opml_recipe(name, recipe).items():
             recipes[sub_name] = r
+    if prefer_submirror:
+        for recipe in recipes.values():
+            _transform_to_submirror(recipe)
     return recipes
+
+
+def _transform_to_submirror(r: dict):
+    for i, u in enumerate(r["urls"]):
+        r["urls"][i] = _transform_url_to_submirror(u)
+
+
+def _transform_url_to_submirror(u: str) -> str:
+    from urllib.parse import urlparse
+    o = urlparse(u)
+    if not o.path.endswith("/feed/atom"):
+        return u
+    if not o.hostname.endswith("mirror.xyz"):
+        return u
+    return u.replace("mirror.xyz", "submirror.xyz").rstrip("/feed/atom")
+
+
+def test_transform_url_to_submirror():
+    assert _transform_url_to_submirror(
+        "https://mirror.xyz/0xBE4fde70B16d2dD454Fa8434E1A01131b90D2880/feed/atom") == "https://submirror.xyz/0xBE4fde70B16d2dD454Fa8434E1A01131b90D2880"
+    assert _transform_url_to_submirror("https://dev.mirror.xyz/feed/atom") == "https://dev.submirror.xyz"
+    assert _transform_url_to_submirror("https://optoutpod.com/blog/feed") == "https://optoutpod.com/blog/feed"
+    assert _transform_url_to_submirror("https://rocry.com/feed/atom") == "https://rocry.com/feed/atom"
 
 
 # change the recipe with opml url to multi recipes
